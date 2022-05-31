623b5ca90d9c78f3e6beed9b6e53633797ac0ce0
Silvan Mueller <silvan.mueller@bossinfo.ch>
2022-05-31 12:48:29 +0200

    add Collection changes and SeedData class

--- SkyblockProject/Models/Collection.cs
+++ SkyblockProject/Models/Collection.cs
@@ -4,12 +4,15 @@
     {
         //ID => Primary Key
         public int Id { get; set; }
-        public string? CollectionTitle { get; set; }
         //fk_CollectionGroup points to Table CollectionGroup, Field ID
-        public string? fk_CollectionGroup { get; set; }
-        //Level shows the Collection Levels
-        public int Level { get; set; }
-        //Experience shows the needed Experience needed for the Level
-        public int Experience { get; set; }
+        public int Fk_CollectionGroup { get; set; }
+        //collectionTitel contains the title of the collection
+        public string? CollectionTitle { get; set; }
+        //collectionLevel shows the Collection Levels
+        public int CollectionLevel { get; set; }
+        //collectionNeededExp shows the needed Experience needed for the Level
+        public int CollectionNeededExp { get; set; }
+        //collectionUnlock shows what you unlock with the Level
+        public string? CollectionUnlock { get; set; }
     }
 }
