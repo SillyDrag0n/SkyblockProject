cc67e6668fb4559bfb44fdda3fb163ba10da7bf9
Silvan Mueller <silvan.mueller@bossinfo.ch>
2022-06-01 13:09:15 +0200

    add several changes

---
+++ SkyblockProject/Controllers/IslandCotroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SkyblockProject.Controllers
+{
+    public class IslandCotroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
